5476bf9b26e4844401e6b02901c01f632c95f8f3
lucca-sa <scherer.lucca@gmail.com>
2024-06-20 23:22:16 -0300

    exception: New Handled Exceptions

--- backend/src/main/java/com/restapi/usermanagement/adapter/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/restapi/usermanagement/adapter/exception/GlobalExceptionHandler.java
@@ -12,11 +12,14 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    private String validationError = "Erro de validação";
+    private String validationError = "Validation Error";
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<Object> handleNoSuchElementException(NoSuchElementException e) {
@@ -43,6 +46,29 @@ public class GlobalExceptionHandler {
         String message = e.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(buildErrorDetails(HttpStatus.BAD_REQUEST, validationError, message));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex,
+            WebRequest request) {
+        String requiredType = ex.getRequiredType().getSimpleName();
+        String error = ex.getName() + " should be of type '" + requiredType + "'.";
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(buildErrorDetails(HttpStatus.BAD_REQUEST, validationError, error));
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<Object> handleNoResourceFoundException(NoResourceFoundException ex, WebRequest request) {
+        Map<String, Object> errorDetails = buildErrorDetails(HttpStatus.NOT_FOUND, "Resource Not Found",
+                ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDetails);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, WebRequest request) {
+        Map<String, Object> errorDetails = buildErrorDetails(HttpStatus.NOT_FOUND, "Handler Not Found",
+                ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDetails);
     }
 
     private Map<String, Object> buildErrorDetails(HttpStatus status, String error, Object message) {
